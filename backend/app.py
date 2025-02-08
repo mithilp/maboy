491a1169b870534167a045b1fd2825bf19519ca4
Anav Bordia <abordia01@gmail.com>
2025-02-08 04:21:18 -0500

    End Text Transcription After 3 Second Pause

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from flask_sock import Sock, ConnectionClosed
 from twilio.twiml.voice_response import VoiceResponse, Start
 from twilio.rest import Client
 import vosk
+import time
 
 app = Flask(__name__)
 sock = Sock(app)
@@ -19,7 +20,7 @@ BS = '\x08'
 @app.route("/call", methods=['GET', 'POST'])
 def call():
     response = VoiceResponse()
-    response.say('Started')
+    response.say('Startypot.')
     start = Start()
     start.stream(url=f'wss://{request.host}/stream')
     response.append(start)
@@ -32,23 +33,36 @@ def call():
 def stream(ws):
     """Receive and transcribe audio stream."""
     rec = vosk.KaldiRecognizer(model, 16000)
+    last_audio_time = time.time()
+    response = ""
     while True:
         message = ws.receive()
         packet = json.loads(message)
         if packet['event'] == 'start':
             print('Streaming is starting')
+            last_audio_time = time.time()
         elif packet['event'] == 'stop':
             print('\nStreaming has stopped')
         elif packet['event'] == 'media':
             audio = base64.b64decode(packet['media']['payload'])
             audio = audioop.ulaw2lin(audio, 2)
             audio = audioop.ratecv(audio, 2, 1, 8000, 16000, None)[0]
+            if not all(byte == 0 for byte in audio):
+                    last_audio_time = time.time()
+
+            if time.time() - last_audio_time > 3: 
+                print("\nNo audio detected for 3 seconds. Ending call.")
+                break
             if rec.AcceptWaveform(audio):
                 r = json.loads(rec.Result())
+                response += CL + r['text'] + ' '
                 print(CL + r['text'] + ' ', end='', flush=True)
             else:
                 r = json.loads(rec.PartialResult())
+                response += CL + r['partial'] + BS * len(r['partial'])
                 print(CL + r['partial'] + BS * len(r['partial']), end='', flush=True)
+    print("RESPONSE: ", response)
+
 
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
